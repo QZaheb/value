a5f1aff64658d44ecfecff3047815846fc076a48
Qais Zaheb <100469755+QZaheb@users.noreply.github.com>
2022-06-29 00:02:56 +0430

    Delete Template Literals directory
    Delete Template Literals directory

--- Template Literals/app.js
+++
@@ -1,8 +0,0 @@
-let name = 'qais';
-let age = 23;
-let sentence = `Hey i am ${name} and i am ${age} years old`;
-
-let sentence2 = "Hey i am " + name + " and i am " + age + " years old";
-
-console.log(sentence);
-console.log(sentence2);
